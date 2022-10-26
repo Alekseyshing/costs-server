8cfb9537a4775d9cc5b5cf759d29f29ea1a6affd
Aleksey <alekseytim@mail.ru>
2022-10-26 12:33:02 +0300

    feat: jwt refresh and access tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,10 +6,11 @@ import { Response } from 'express';
 import { RegistrationGuard } from './guards/registration.guard';
 import { LoginUserDto } from './dto/login-user.dto';
 import { LoginGuard } from './guards/login.guards';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private usersService: UsersService) { }
+  constructor(private usersService: UsersService, private authService: AuthService) { }
 
   @UseGuards(LoginGuard)
   @Post('login')
@@ -18,8 +19,11 @@ export class AuthController {
     @Res() res: Response,
   ) {
     const user = await this.usersService.login(loginUserDto);
+
+    const access = await this.authService.generateAccessToken(user);
+    const refresh = await this.authService.generateRefreshToken(user._id as string);
     res.statusCode = HttpStatus.OK;
-    return res.send({ username: user.username });
+    return res.send({ ...access, ...refresh, username: user.username });
   }
 
   @UseGuards(RegistrationGuard)
