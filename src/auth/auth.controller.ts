198141e1d1992f99dcff7cf222ef82f3ad4b9483
Aleksey <alekseytim@mail.ru>
2022-10-26 11:37:50 +0300

    feat: login OK

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,10 +4,23 @@ import { UsersService } from 'src/users/users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { Response } from 'express';
 import { RegistrationGuard } from './guards/registration.guard';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LoginGuard } from './guards/login.guards';
 
 @Controller('auth')
 export class AuthController {
   constructor(private usersService: UsersService) { }
+
+  @UseGuards(LoginGuard)
+  @Post('login')
+  async loginUser(
+    @Body() loginUserDto: LoginUserDto,
+    @Res() res: Response,
+  ) {
+    const user = await this.usersService.login(loginUserDto);
+    res.statusCode = HttpStatus.OK;
+    return res.send({ username: user.username });
+  }
 
   @UseGuards(RegistrationGuard)
   @Post('registration')
