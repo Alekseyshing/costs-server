9d4bd44fdcd7f281fc0f9947d15ca4c868998ba7
Aleksey <alekseytim@mail.ru>
2022-10-26 11:09:54 +0300

    feat: registration ok

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UsersSchema } from 'src/schemas/users.schema';
+import { UsersService } from './users.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UsersSchema }]),
   ],
+  providers: [UsersService],
+  exports: [UsersService],
 })
 // eslint-disable-next-line prettier/prettier
 export class UsersModule { }
